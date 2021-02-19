17f748144527790152cbe532adc2ef11a510540f
mdu830 <mdu830@yahoo.com>
2021-02-19 13:26:00 -0500

    finished intern.test

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,3 +1,21 @@
 const Employee = require("../lib/employee");
 const Intern = require("../lib/intern");
 
+// check office number input
+test("Checking school input/output", () => {
+    const input = "UNC Chapel Hill";
+    const user = new Intern("John", 12345, "longjohnson@domain.com", input);
+    expect(user.school).toBe(input);
+});
+// check if getRole() returns "Intern"
+test("Checking if getRole() returns the users input", () => {
+    const choice = "Intern";
+    const user = new Intern("jimmy", 12345, "jimmy@domain.com");
+    expect(user.getRole()).toBe(choice);
+});
+// check if getSchool() returns user input
+test("Checking if getSchool() returns the users input", () => {
+    const input = "UNC Chapel Hill";
+    const user = new Intern("jimmy", 12345, "jimmy@domain.com", input);
+    expect(user.getSchool()).toBe(input);
+});
